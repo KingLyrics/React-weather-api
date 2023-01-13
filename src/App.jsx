ccc87d9d9f882272cf3e76155eb45108ab27ee7f
KingLyrics <ekom338.com@gmail.com>
2023-01-13 22:42:56 +0100

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
-function App() {
-  return <div className="App"></div>;
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_KEY = "6acb1a5237c72587dbd6c3935ad37940";
+
+function Weather() {
+  const [weather, setWeather] = useState({});
+  const [location, setLocation] = useState("Moroco");
+
+  useEffect(() => {
+    async function fetchWeather() {
+      const result = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`
+      );
+
+      setWeather(result.data);
+    }
+
+    fetchWeather();
+  }, [location]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setLocation(e.target.elements.location.value);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="location" placeholder="Enter a location" />
+        <button>Submit</button>
+      </form>
+      {weather.main ? (
+        <div>
+          <p>Temperature: {weather.main.temp}</p>
+          <p>Humidity: {weather.main.humidity}</p>
+          <p>Description: {weather.weather[0].description}</p>
+        </div>
+      ) : (
+        <p>Loading weather data...</p>
+      )}
+    </div>
+  );
 }
 
-export default App;
+export default Weather;
